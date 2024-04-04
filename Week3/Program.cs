6d7ccdbd1efb070c62482c908618ef93f336522e
KaurGR <kaur-gabriel.rajasalu@tktk.ee>
2024-04-04 12:39:35 +0300

    Add Week2

---
+++ Week3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Even / odd numbers
+
+Console.WriteLine("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 2;
+
+if (result != 0) //!= is not
+{
+    Console.WriteLine("This number is odd.");
+}
+else
+{
+    Console.WriteLine("This number is even.");
+}
